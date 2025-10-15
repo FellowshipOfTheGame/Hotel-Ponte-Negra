50d7b6a461ba5b0b28baeee4eddd5d1d2c7a1e3e
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2025-10-15 14:46:26 -0300

    Corrigi a camera e unifiquei a logica de interação e  a logica de ação do inimigo

--- scripts/camera/cameraControllerStatic.gd
+++ scripts/camera/cameraControllerStatic.gd
@@ -1,33 +1,53 @@
-# ControladorCamera.gd
+# ControladorCameraIsometrica.gd
 extends Camera3D
 
+@export_group("Alvo e Configurações")
 @export var alvo_jogador: Node3D
 
-var objeto_obstruindo_atualmente: Obstacle = null
+@export var offset_isometrico: Vector3 = Vector3(0, 8, 10)
+
+
+@export var suavidade: float = 5.0
+
+@export_group("Detecção de Obstáculos")
+var objeto_obstruindo_atualmente: Node = null
 
 func _physics_process(delta: float) -> void:
 	if not alvo_jogador:
 		return
+	var posicao_desejada = alvo_jogador.global_position + offset_isometrico
+
+	if suavidade > 0:
+		global_position = global_position.lerp(posicao_desejada, delta * suavidade)
+	else:
+		global_position = posicao_desejada
+
+	look_at(alvo_jogador.global_position)
+
 
 	var space_state = get_world_3d().direct_space_state
 	var inicio_raio = self.global_position
-	var fim_raio = alvo_jogador.global_position
+	var fim_raio = alvo_jogador.global_position + Vector3.UP * 1.0 
+	
 	var query = PhysicsRayQueryParameters3D.create(inicio_raio, fim_raio)
+	query.exclude = [alvo_jogador]
+	
 	var resultado = space_state.intersect_ray(query)
 
 	if resultado:
-		var colisor = resultado.collider as Node
-		if colisor is Obstacle:
+		var colisor = resultado.collider
+		if colisor.has_method("tornar_transparente"):
 			if colisor != objeto_obstruindo_atualmente:
-				if objeto_obstruindo_atualmente:
-					objeto_obstruindo_atualmente.tornar_opaco()
+				limpar_obstaculo_anterior() 
+				
 				colisor.tornar_transparente()
 				objeto_obstruindo_atualmente = colisor
 		else:
-			if objeto_obstruindo_atualmente:
-				objeto_obstruindo_atualmente.tornar_opaco()
-				objeto_obstruindo_atualmente = null
+			limpar_obstaculo_anterior()
 	else:
-		if objeto_obstruindo_atualmente:
-			objeto_obstruindo_atualmente.tornar_opaco()
-			objeto_obstruindo_atualmente = null
+		limpar_obstaculo_anterior()
+		
+func limpar_obstaculo_anterior():
+	if is_instance_valid(objeto_obstruindo_atualmente) and objeto_obstruindo_atualmente.has_method("tornar_opaco"):
+		objeto_obstruindo_atualmente.tornar_opaco()
+	objeto_obstruindo_atualmente = null
--- scripts/moveEnemy.gd
+++ scripts/moveEnemy.gd
@@ -1,8 +1,12 @@
 extends CharacterBody3D
 
 @export var speed: float = 5.0
+@onready var vision_shapecast: ShapeCast3D = $ActionComponent
 
 var player: Node3D = null
+
+var attack_cooldown = 2.0
+var can_attack = true
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
@@ -12,10 +16,21 @@ func _physics_process(delta: float) -> void:
 		print_debug("Inimigo não conseguiu encontrar o nó do jogador. Verifique o grupo 'Player'.")
 		return 
 
-	var direcao = global_position.direction_to(player.global_position)
+	isPlayerClose()
+	if can_attack == true:
+		var direcao = global_position.direction_to(player.global_position)
+		velocity = velocity.lerp(direcao * speed, delta * 2.0)
+		look_at(player.global_position, Vector3.UP)
+		move_and_slide()
 	
-	velocity = velocity.lerp(direcao * speed, delta * 2.0)
-	
-	look_at(player.global_position, Vector3.UP)
-	
-	move_and_slide()
+func isPlayerClose() -> void:
+	if vision_shapecast.is_colliding():
+		var collider = vision_shapecast.get_collider(0)
+		if collider and collider.is_in_group("Player") and can_attack:
+			attack(collider)
+			
+func attack(target):
+	print("Inimigo detectou ", target.name, " com um BOXCAST e atacou!")
+	can_attack = false
+	await get_tree().create_timer(attack_cooldown).timeout
+	can_attack = true
