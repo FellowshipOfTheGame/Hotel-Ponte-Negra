c1a493b8024b012e2e43b7574af0f02aad1bfcab
Henrique Garcia Gomes do Ó <87249985+henriqueggdo@users.noreply.github.com>
2025-10-15 22:06:20 -0300

    Correção de Merge

--- scripts/objects/types/interactable.gd
+++ scripts/objects/types/interactable.gd
@@ -1,6 +1,6 @@
 extends StaticBody3D
 class_name Interactable
 
-func _on_interact(interactor: Node):
+func _on_interact(_interactor: Node):
 	print("Este objeto interativo não tem uma ação definida.")
 	pass # Classes filhas vão sobrescrever isso
