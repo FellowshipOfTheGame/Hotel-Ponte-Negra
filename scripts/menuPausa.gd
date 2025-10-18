f097a5952fe2e2a90931e4cf5cf67dbbc1d38c0b
Mimol <miguelpsmolina@usp.br>
2025-10-18 16:18:27 -0300

    menu de pausa

--- scripts/menuPausa.gd
+++ scripts/menuPausa.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-func _on_jogar_pressed() -> void:
+func _on_voltar_pressed() -> void:
 	get_tree().paused=false
 	self.visible=false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 
 
-func _on_sair_pressed() -> void:
+func _on_menu_pressed() -> void:
 	get_tree().paused=false
 	get_tree().change_scene_to_file("res://scenes/menus/menuPrincipal.tscn")
