237f6a1ea1dd7ad0673feed1dc6be114cea16e8a
Mimol <miguelpsmolina@usp.br>
2025-11-10 21:50:40 -0300

    Update definirControle.gd

--- scripts/definirControle.gd
+++ scripts/definirControle.gd
@@ -1,9 +1,8 @@
 extends Button
-var tecla:Key
+
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventKey:if has_focus():
-		tecla=event.keycode
 		text=OS.get_keycode_string(event.keycode)
 		release_focus()
 		get_viewport().set_input_as_handled()
