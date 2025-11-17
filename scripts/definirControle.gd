98e98d4038231f764b24c6878c820dcb25309b0d
Mimol <miguelpsmolina@usp.br>
2025-11-17 14:59:16 -0300

    valores iniciais no menu opcoes

--- scripts/definirControle.gd
+++ scripts/definirControle.gd
@@ -1,4 +1,10 @@
 extends Button
+@export var acao:String
+
+func _ready() -> void:
+	if InputMap.has_action(acao):
+		text=InputMap.action_get_events(acao)[0].as_text().trim_suffix(" (Physical)")
+	if!text:text="nao definido"
 
 
 func _input(event: InputEvent) -> void:
--- scripts/menu_opcoes.gd
+++ scripts/menu_opcoes.gd
@@ -1,12 +1,10 @@
 extends Control
 
-var anterior:Node
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventAction:match event.action:
-		"ui_cancel":
-			get_viewport().set_input_as_handled()
-			voltar()
+	if event.is_action_pressed("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		voltar()
 
 func voltar() -> void:
 	$'..'/Container.visible=true
