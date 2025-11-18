d113554d869f94c252da912e24975050e6a0537a
Mimol <miguelpsmolina@usp.br>
2025-11-18 14:39:15 -0300

    Atualizado edição de controles

--- scripts/definirControle.gd
+++ scripts/definirControle.gd
@@ -2,13 +2,24 @@ extends Button
 @export var acao:String
 
 func _ready() -> void:
-	if InputMap.has_action(acao):
-		text=InputMap.action_get_events(acao)[0].as_text().trim_suffix(" (Physical)")
-	if!text:text="nao definido"
+	if!InputMap.has_action(acao):text="nao definido";return
+	var event = InputMap.action_get_events(acao)[0]
+	if event is InputEventJoypadMotion:
+		text = event.as_text().get_slice("(",1).get_slice(",",0)
+	elif event is InputEventKey:
+		text = event.as_text().get_slice(" ",0)
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:if has_focus():
-		text=OS.get_keycode_string(event.keycode)
+func _input(event: InputEvent) -> void: if has_focus():
+	if!InputMap.has_action(acao):return
+	InputMap.action_erase_events(acao)
+	InputMap.action_add_event(acao,event)
+	if event is InputEventKey:
+		text = event.as_text()
+		release_focus()
+		get_viewport().set_input_as_handled()
+	elif event is InputEventMouseButton:if event.is_pressed():
+		event.double_click = false
+		text = event.as_text()
 		release_focus()
 		get_viewport().set_input_as_handled()
--- scripts/stateMachine/state.gd
+++ scripts/stateMachine/state.gd
@@ -2,16 +2,17 @@ extends Node
 
 class_name State
 
+@warning_ignore("unused_signal")
 signal Transitioned(state, new_state_name)
 
 func Enter():
 	pass
-	
+
 func Exit():
 	pass
-		
+
 func Update(_delta: float):
 	pass
-	
+
 func Physics_Update(_delta: float):
 	pass
