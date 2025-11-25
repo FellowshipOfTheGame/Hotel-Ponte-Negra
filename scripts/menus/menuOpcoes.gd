481ed33f738748dde584511208c4d826e9fde2c6
Mimol <miguelpsmolina@usp.br>
2025-11-25 09:25:57 -0300

    Sair do menu opcoes salva os controles em um arquivo

--- scripts/menus/menuOpcoes.gd
+++ scripts/menus/menuOpcoes.gd
@@ -7,5 +7,15 @@ func _input(event: InputEvent) -> void:
 		voltar()
 
 func voltar() -> void:
+	const  confPath = "user://config"
+	var conf = ConfigFile.new()
+	conf.load(confPath)
+	for acao in InputMap.get_actions():
+		if!(InputMap.action_get_events(acao).is_empty()
+			or acao.begins_with("ui_")):
+			var event := InputMap.action_get_events(acao)[0]
+			conf.set_value("controles",acao,event.as_text().get_slice(" ",0))
+	conf.save(confPath)
+
 	$'..'/Container.visible=true
 	queue_free()
