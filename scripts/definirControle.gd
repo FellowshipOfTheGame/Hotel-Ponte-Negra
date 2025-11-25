b4573152bad070bdf26169c8ddfcbf93e5ce8838
Mimol <miguelpsmolina@usp.br>
2025-11-25 11:04:44 -0300

    Finalizado arquivo de configurações

--- scripts/definirControle.gd
+++ scripts/definirControle.gd
@@ -1,19 +1,16 @@
 extends Button
-@export var acao:String
 
 func _ready() -> void:
-	if!InputMap.has_action(acao):text="nao definido";return
-	var event = InputMap.action_get_events(acao)[0]
+	var event = InputMap.action_get_events(name)[0]
 	if event is InputEventJoypadMotion:
 		text = event.as_text().get_slice("(",1).get_slice(",",0)
 	elif event is InputEventKey:
 		text = event.as_text().get_slice(" ",0)
 
 
-func _input(event: InputEvent) -> void: if has_focus():
-	if InputMap.has_action(acao):
-		InputMap.action_erase_events(acao)
-		InputMap.action_add_event(acao,event)
+func _input(event: InputEvent) -> void:if has_focus():
+	InputMap.action_erase_events(name)
+	InputMap.action_add_event(name,event)
 	if event is InputEventKey:
 		text = event.as_text()
 		release_focus()
--- scripts/menus/menuOpcoes.gd
+++ scripts/menus/menuOpcoes.gd
@@ -2,6 +2,14 @@ extends Control
 
 const confPath = "user://config"
 
+func _init() -> void:
+	var conf := ConfigFile.new();conf.load(confPath)
+	for acao in conf.get_section_keys("controles"):
+		var key :String= conf.get_value("controles",acao)
+		var event:=InputMap.action_get_events(acao)[0]
+		if event is InputEventKey:
+			event.keycode=OS.find_keycode_from_string(key)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_viewport().set_input_as_handled()
@@ -14,7 +22,12 @@ func voltar() -> void:
 		if!(InputMap.action_get_events(acao).is_empty()
 			or acao.begins_with("ui_")):
 			var event := InputMap.action_get_events(acao)[0]
-			conf.set_value("controles",acao,event.as_text().get_slice(" ",0))
+			var text := event.as_text()
+			if event is InputEventKey:
+				text=text.get_slice(" ",0)
+			elif event is InputEventJoypadMotion:
+				text=text.get_slice("(",1).get_slice(",",0)
+			conf.set_value("controles",acao,text)
 	conf.save(confPath)
 
 	$'..'/Container.visible=true
