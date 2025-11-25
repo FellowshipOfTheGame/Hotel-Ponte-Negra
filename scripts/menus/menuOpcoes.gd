9dd377a9539744ae16611b0f429be73e17d5b186
Mimol <miguelpsmolina@usp.br>
2025-11-25 13:11:38 -0300

    Update menuOpcoes.gd

--- scripts/menus/menuOpcoes.gd
+++ scripts/menus/menuOpcoes.gd
@@ -2,7 +2,8 @@ extends Control
 
 const confPath = "user://config"
 
-func _init() -> void:
+func _init() -> void: # funcao load()
+	if!FileAccess.file_exists(confPath):return
 	var conf := ConfigFile.new();conf.load(confPath)
 	for acao in conf.get_section_keys("controles"):
 		var key :String= conf.get_value("controles",acao)
@@ -16,19 +17,22 @@ func _input(event: InputEvent) -> void:
 		voltar()
 
 func voltar() -> void:
-	var conf := ConfigFile.new()
-	conf.load(confPath)
-	for acao in InputMap.get_actions():
-		if!(InputMap.action_get_events(acao).is_empty()
-			or acao.begins_with("ui_")):
-			var event := InputMap.action_get_events(acao)[0]
-			var text := event.as_text()
-			if event is InputEventKey:
-				text=text.get_slice(" ",0)
-			elif event is InputEventJoypadMotion:
-				text=text.get_slice("(",1).get_slice(",",0)
-			conf.set_value("controles",acao,text)
-	conf.save(confPath)
-
+	save()
 	$'..'/Container.visible=true
 	queue_free()
+
+func save()->void:
+	var conf := ConfigFile.new();conf.load(confPath)
+
+	for acao in InputMap.get_actions():
+		if acao.begins_with("ui_"): return
+		if InputMap.action_get_events(acao).is_empty(): return
+
+		var event := InputMap.action_get_events(acao)[0]
+		var text := event.as_text()
+		if event is InputEventKey:
+			text=text.get_slice(" ",0)
+		elif event is InputEventJoypadMotion:
+			text=text.get_slice("(",1).get_slice(",",0)
+		conf.set_value("controles",acao,text)
+	conf.save(confPath)
