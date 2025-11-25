d4c5e08d170cfd2f1873a8c03e6464b89b78ffc8
Mimol <miguelpsmolina@usp.br>
2025-11-25 17:47:55 -0300

    Update base.gd

--- scripts/base.gd
+++ scripts/base.gd
@@ -1,9 +1,8 @@
 extends Node3D
 
-const pausa:PackedScene=preload("res://scenes/menus/menuPausa.tscn")
-
+const menuPausa :PackedScene= preload("res://scenes/menus/menuPausa.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		get_tree().root.add_child(pausa.instantiate())
+		get_tree().root.add_child(menuPausa.instantiate())
 		get_viewport().set_input_as_handled()
