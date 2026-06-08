aba14c1c8574fd63d9e787a77ca19429a5e99f13
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2026-06-07 22:05:19 -0300

    Fiz um efeito sonoro pra chave

--- scripts/objects/key/chave.gd
+++ scripts/objects/key/chave.gd
@@ -2,7 +2,11 @@ extends Interactable
 
 signal get_key()
 
+@onready var SomChave = $AudioStreamPlayer3D
+
 func _on_interact(_interactor: Node):
 	print("Pegou a chave")
+	SomChave.play()
 	get_key.emit()
+	await SomChave.finished
 	queue_free()
